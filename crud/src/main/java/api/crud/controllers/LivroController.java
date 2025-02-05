1798ca4a44c81b79501f99cbb30645f235807058
Pedro Augusto <pedroaugustodev4@gmail.com>
2025-02-05 10:10:37 -0300

    fazendo datas

--- crud/src/main/java/api/crud/controllers/LivroController.java
+++ crud/src/main/java/api/crud/controllers/LivroController.java
@@ -3,19 +3,16 @@ package api.crud.controllers;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.net.http.HttpResponse.ResponseInfo;
-
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 
 @RestController
 @RequestMapping("/livros")
-public class Livro {
+public class LivroController {
     
     @GetMapping
     public ResponseEntity getLivros() {
---
+++ crud/src/main/java/api/crud/models/Category.java
@@ -0,0 +1,10 @@
+package api.crud.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "category")
+public class Category {
+    
+}
