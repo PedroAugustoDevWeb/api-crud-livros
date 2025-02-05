d8d808e3e624c6f024f3fc994f3b2b8028817a11
Pedro Augusto <pedroaugustodev4@gmail.com>
2025-02-05 10:16:10 -0300

    terminando datas

--- crud/src/main/java/api/crud/models/Category.java
+++ crud/src/main/java/api/crud/models/Category.java
@@ -1,10 +1,27 @@
 package api.crud.models;
 
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Table(name = "category")
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Category {
-    
+    private String name;
+
+    private String desc;
+
+    @OneToOne(mappedBy = "category", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Livros> livros;
 }
--- crud/src/main/java/api/crud/models/Livros.java
+++ crud/src/main/java/api/crud/models/Livros.java
@@ -4,10 +4,20 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Table(name = "tb_livros")
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Livros {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -18,5 +28,9 @@ public class Livros {
     private String desc;
 
     private int ano_lanca;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private Category category;
 
 }
